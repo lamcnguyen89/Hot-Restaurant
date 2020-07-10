7c8d2528db68d015e960223086ba3c94f577819e
Jody Eggleston <jmeggles@gmail.com>
2020-07-10 09:05:56 -0400

    added routes for home page, reserve, and tables

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ var PORT = process.env.PORT || 3000;
 var reservations = [];
 var waitlist = [];
 
+// first scope of code that fires... 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));  
+});
+
+// second scope of code that fires...this method allows user to add info to the page (fill in form)
+// server is listening for the request
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));  
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));  
+});
+
+// json sends over a type of object (can be an array or any object you want to send, its seen then strinify'ed)
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations);                    
+});
+
 
 
 
