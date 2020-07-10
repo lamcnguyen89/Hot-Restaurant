32b9ee9f6c965af5183771ded9f58f5c9c9422cb
RichT <rrtrenchf@gmail.com>
2020-07-10 11:00:39 -0400

    fixed response

--- server.js
+++ server.js
@@ -2,17 +2,34 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const { table } = require("console");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 // port for heroku access
 var PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Arrays for reservations and waitlist
 var reservations = [];
 var waitlist = [];
+var availTables = 4
+//for loop for each table
 
+function tablepush(){
+for (i=0; i<=availTables.length;i++){
+  if(i<=4) { 
+    reservations.push[i]
+  }else{
+    waitlist.push[i]
+  }
+}
+}
+tablepush()
+console.log(reservations)
 // first scope of code that fires... 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));  
@@ -38,6 +55,11 @@ app.get("/api/tables", function(req, res) {
 app.get("/api/waitlist", function(req, res) {
   return res.json(waitlist);
 })
+
+app.post("/reserve", function(req, res) {
+  console.log(req.body)
+  res.json(req.body)
+});
 
 
 
