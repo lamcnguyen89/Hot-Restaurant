d0aa6837b4d136193cec5b2478eacd15c2907fc2
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-10 11:00:43 -0400

    Removed hardocding

--- server.js
+++ server.js
@@ -12,11 +12,25 @@ var PORT = process.env.PORT || 3000;
 // Arrays for reservations and waitlist
 var reservations = [];
 var waitlist = [];
+var AvailTables = 4
+
+function tablepush(){
+  for (i=0; i<=4;i++){
+    if(i<=4) { 
+      reservations.push[i]
+    }else{
+      waitlist.push[i]
+    }
+  }
+  }
+tablepush
 
 // first scope of code that fires... 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));  
 });
+
+
 
 // second scope of code that fires...this method allows user to add info to the page (fill in form)
 // server is listening for the request
@@ -39,7 +53,9 @@ app.get("/api/waitlist", function(req, res) {
   return res.json(waitlist);
 })
 
-
+app.post("/reserve", function(req, res) {
+  console.log(req.body); 
+});
 
 // Starts the server to begin listening
 // =============================================================
